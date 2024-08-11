53ed10a9d3bc793563fe077d1b5472de251d4694
Darek <DRodzewicz@gmail.com>
2024-08-11 22:33:12 +0200

    Setup shadcn and tailwind

--- frontend/src/main/App.tsx
+++ frontend/src/main/App.tsx
@@ -2,7 +2,7 @@ import { Link, Outlet } from "react-router-dom";
 
 const App = () => {
     return (
-        <div className="bg-red-500">
+        <div>
             Hello worlda
             <h1 className="text-3xl font-bold underline">Hello world!</h1>
             <Link to="/locations">locations</Link>
